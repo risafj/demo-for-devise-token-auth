1a82c3b938d96d8c87a6a064756f1e688f13f6cb
Risa Fujii <risaf1231@gmail.com>
2020-01-01 09:39:17 +0900

    Generate book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_01_001209) do
+ActiveRecord::Schema.define(version: 2020_01_01_003324) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
