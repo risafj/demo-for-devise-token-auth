6d22380979b405d2be3c9ceeb9b7423485de57db
Risa Fujii <risaf1231@gmail.com>
2020-01-01 10:01:03 +0900

    Drop :trackable from the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,12 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  # NOTE: Deleted :trackable (for tracking the last signed-in ip address, date/time etc.) since it was dropped from the Devise default.
+  # Refer to the links below for details:
+  # https://stackoverflow.com/questions/55735895/nomethoderror-undefined-method-current-sign-in-at-for-user0x000055ce01dcf0a
+  # https://www.bountysource.com/issues/75245078-cant-log-in-nomethoderror-undefined-method-current_sign_in_at-for-user-0x000055e053c79c58
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
